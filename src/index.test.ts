70306388815ee0083f35d040ace06d58b0063e49
ootidea <ootidea@gmail.com>
2023-07-13 21:05:20 +0900

    fix: cannot narrowing

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,13 +2,17 @@ import { expect, test } from 'vitest'
 import { Failure, Result, Success } from './index'
 
 test('Constructor', () => {
-  const success: Result<number> = new Success(123)
-  expect(success.value).toBe(123)
+  const success = new Success(123) as Result<number>
   expect(success.isSuccess).toBe(true)
   expect(success.isFailure).toBe(false)
+  if (success.isSuccess) {
+    expect(success.value).toBe(123)
+  }
 
-  const failure: Result<number> = new Failure(new Error('error'))
-  expect(failure.error.message).toBe('error')
+  const failure = new Failure(new Error('error')) as Result<number>
   expect(failure.isSuccess).toBe(false)
   expect(failure.isFailure).toBe(true)
+  if (failure.isFailure) {
+    expect(failure.error.message).toBe('error')
+  }
 })
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ export type Result<T, E extends Error = Error> = Success<T> | Failure<E>
 
 export class Success<T> {
   constructor(public value: T) {}
-  isSuccess = true
-  isFailure = false
+  readonly isSuccess = true
+  readonly isFailure = false
 }
 
 export class Failure<E extends Error> {
   constructor(public error: E) {}
-  isSuccess = false
-  isFailure = true
+  readonly isSuccess = false
+  readonly isFailure = true
 }
