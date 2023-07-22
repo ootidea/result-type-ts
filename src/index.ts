125866ff56f466db81f3e6de7bd0d1f5759f8958
ootidea <ootidea@gmail.com>
2023-07-22 16:37:24 +0900

    feat: define map method

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export class Success<const T> {
   public readonly error?: never
   readonly isSuccess = true
   readonly isFailure = false
+
+  map<T2, E>(f: (value: T) => T2, g: (_: never) => E): Success<T2> {
+    return new Success(f(this.value))
+  }
 }
 
 export class Failure<const E> {
@@ -10,6 +14,10 @@ export class Failure<const E> {
   public readonly value?: never
   readonly isSuccess = false
   readonly isFailure = true
+
+  map<T, E2>(f: (_: never) => T, g: (value: E) => E2): Failure<E2> {
+    return new Failure(g(this.error))
+  }
 }
 
 export type Result<T, E = unknown> = Success<T> | Failure<E>
