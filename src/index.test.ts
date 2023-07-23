fdeaa3ec6d5dda1249f87bc02a4d8908459e5886
ootidea <ootidea@gmail.com>
2023-07-23 17:21:56 +0900

    test: change test order

--- src/index.test.ts
+++ src/index.test.ts
@@ -15,18 +15,6 @@ test('Properties', () => {
   expect(failure.error).toBe('error')
 })
 
-test('tryCatch', () => {
-  const success = Result.tryCatch(() => 123)
-  expect(success.isSuccess).toBe(true)
-  expect(success.value).toBe(123)
-
-  const failure = Result.tryCatch(() => {
-    throw new Error('error')
-  })
-  expect(failure.isFailure).toBe(true)
-  expect(failure.error instanceof Error && failure.error.message).toBe('error')
-})
-
 test('ifSuccess', () => {
   const success = Result.success(123) as Result<number>
   expect(success.ifSuccess((value) => -value)).toBe(-123)
@@ -41,4 +29,16 @@ test('ifFailure', () => {
 
   const failure = Result.failure(new Error('error')) as Result<number, Error>
   expect(failure.ifFailure((error) => error.message)).toBe('error')
+})
+
+test('tryCatch', () => {
+  const success = Result.tryCatch(() => 123)
+  expect(success.isSuccess).toBe(true)
+  expect(success.value).toBe(123)
+
+  const failure = Result.tryCatch(() => {
+    throw new Error('error')
+  })
+  expect(failure.isFailure).toBe(true)
+  expect(failure.error instanceof Error && failure.error.message).toBe('error')
 })
